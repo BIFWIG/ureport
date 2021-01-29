81224e6c96cdcdeeb791c02d82386ea490dabeda
bif <wf19941211@gmail.com>
2021-01-29 10:34:17 +0800

    dev web demo

---
+++ web-demo/src/main/java/cn/bif/Application.java
@@ -0,0 +1,17 @@
+package cn.bif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Ted Wang
+ * @created 2021/1/29 10:31 上午
+ */
+@SpringBootApplication
+public class Application {
+    
+    
+    public static void main (String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
